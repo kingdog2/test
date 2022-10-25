a99d209f2679d5a212b885865cbb8ad02fcfec47
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2022-10-25 23:48:52 +0800

    Add files via upload

---
+++ Ex01_Button02_onClick01(use onClick)/app/src/main/java/com/example/ex01_button02_onclick01/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.ex01_button02_onclick01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView txtMsg;
+    Button btn1, btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         txtMsg = this.findViewById(R.id.txtMsg);
+        btn1 = this.findViewById(R.id.btn1);
+        btn2 = this.findViewById(R.id.btn2);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+//        int id = view.getId();
+//        switch (id){
+//            case R.id.btn1:
+//
+//                break;
+//            case R.id.btn2:
+//
+//                break;
+//        }
+        //Button btn = (Button)view;
+        String str = ((Button)view).getText().toString();
+        //txtMsg.setText(btn.getText()+" is ckicked");
+        txtMsg.setText(str+" is ckicked");
+
+    }
+}
